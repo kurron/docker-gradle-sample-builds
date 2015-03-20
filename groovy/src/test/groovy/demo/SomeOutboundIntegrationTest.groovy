28583c797d423b8e3d61d9fb2b682a31f3867ce2
Ron Kurr <kurr@kurron.org>
2015-03-20 18:54:17 -0400

    Outbound integration test is working.

---
+++ groovy/src/test/groovy/demo/SomeOutboundIntegrationTest.groovy
@@ -0,0 +1,25 @@
+package demo
+
+import groovy.util.logging.Slf4j
+import org.junit.experimental.categories.Category
+import org.kurron.categories.ComponentTest
+import org.kurron.categories.OutboundIntegrationTest
+import org.springframework.boot.test.SpringApplicationContextLoader
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+@Category( OutboundIntegrationTest )
+@Slf4j
+@ContextConfiguration( classes = Application, loader = SpringApplicationContextLoader )
+class SomeOutboundIntegrationTest extends Specification{
+
+    def 'contextLoads'() {
+        given:
+        def foo = true
+
+        expect:
+        log.info( 'some outbound integration test ran!' )
+        foo
+    }
+
+}
