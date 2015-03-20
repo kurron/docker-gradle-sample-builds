7f394ca2e9c1798eb3f8abd7b132af75911fdcd0
Ron Kurr <kurr@kurron.org>
2015-03-20 16:46:48 -0400

    Made the component test have Spring run to wire up the beans.

--- groovy/src/test/groovy/demo/DemoApplicationTests.groovy
+++ groovy/src/test/groovy/demo/DemoApplicationTests.groovy
@@ -3,10 +3,13 @@ package demo
 import groovy.util.logging.Slf4j
 import org.junit.experimental.categories.Category
 import org.kurron.categories.ComponentTest
+import org.springframework.boot.test.SpringApplicationContextLoader
+import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
 
 @Category( ComponentTest )
 @Slf4j
+@ContextConfiguration( loader = SpringApplicationContextLoader )
 class DemoApplicationTests extends Specification{
 
     def 'contextLoads'() {
