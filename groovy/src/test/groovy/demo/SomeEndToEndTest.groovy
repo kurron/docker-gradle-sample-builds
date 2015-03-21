f908851f1d5eb72dabbabb327bbe0b99e691bbfd
Ron Kurr <kurr@kurron.org>
2015-03-20 20:43:06 -0400

    end-to-end test is working.

---
+++ groovy/src/test/groovy/demo/SomeEndToEndTest.groovy
@@ -0,0 +1,26 @@
+package demo
+
+import groovy.util.logging.Slf4j
+import org.junit.experimental.categories.Category
+import org.kurron.categories.EndToEndTest
+import org.springframework.boot.test.SpringApplicationContextLoader
+import org.springframework.boot.test.WebIntegrationTest
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+@Category( EndToEndTest )
+@Slf4j
+@ContextConfiguration( classes = Application, loader = SpringApplicationContextLoader )
+@WebIntegrationTest( randomPort = true )
+class SomeEndToEndTest extends Specification{
+
+    def 'contextLoads'() {
+        given:
+        def foo = true
+
+        expect:
+        log.info( 'some end-to-end test ran!' )
+        foo
+    }
+
+}
