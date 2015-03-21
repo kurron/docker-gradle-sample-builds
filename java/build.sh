3592dcfcb170f465a3b8702fef791af41dc6861d
Ron Kurr <kurr@kurron.org>
2015-03-20 22:35:34 -0400

    Initial commit

---
+++ java/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# run this script giving it a single argument eg. ./build.sh tasks
+
+# you have to let the container write to your home folder or you cannot see the generated IDE files or cached libraries
+docker run --name="Gradle" --volume $(pwd):/pwd --volume $HOME:$HOME --user=$(id -u $(whoami)):$(id -g $(whoami)) --env HOME=$HOME --env GRADLE_USER_HOME=$HOME --rm --attach stdout --attach stderr --interactive kurron/gradle "$@"
