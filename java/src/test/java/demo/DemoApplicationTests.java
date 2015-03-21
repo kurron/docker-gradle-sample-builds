e0548f32d6edd592237f6d26d1aeff3f2cd72eb7
Ron Kurr <kurr@kurron.org>
2015-03-20 22:45:56 -0400

    Made it a component test but Gradle is still running it.

--- java/src/test/java/demo/DemoApplicationTests.java
+++ java/src/test/java/demo/DemoApplicationTests.java
@@ -1,10 +1,13 @@
 package demo;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import org.kurron.categories.ComponentTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@Category( ComponentTest.class )
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 public class DemoApplicationTests {
